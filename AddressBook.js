a1939caad4ecde469c1b9d498a2981b5d858dbd6
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-15 20:34:55 +0530

    Added logic to Validate Contact Details

--- AddressBook.js
+++ AddressBook.js
@@ -9,14 +9,49 @@ class AddressBook {
     email;
 
     constructor (...params) {
-        this.firstName = params[0];
-        this.lastName = params[1];
-        this.address = params[2];
-        this.city = params[3];
-        this.state = params[4];
-        this.zip = params[5];
-        this.phoneNumber = params[6];
-        this.email = params[7];
+        let regex_for_name = new RegExp('[A-Z]{1}[a-z]{3,}');
+        let regex_for_address = new RegExp('[A-Z]{1}[a-z]');
+        let regex_for_zip = new RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$');
+        let regex_for_phoneNumber = new RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+        let regex_for_email = new RegExp('^[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-])*@[A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*$');
+
+        if(regex_for_name.test(params[0]) && regex_for_name.test(params[1])){
+            this.firstName = params[0];
+            this.lastName = params[1];
+        }
+        else{
+            throw "Invalid Name";
+        }
+
+        if (regex_for_address.test(params[2]) && regex_for_address.test(params[3]) && regex_for_address.test(params[4])) {
+            this.address = params[2];
+            this.city = params[3];
+            this.state = params[4];
+        }
+        else{
+            throw "Invalid Address";
+        }
+
+        if (regex_for_zip.test(params[5])) {
+            this.zip = params[5];
+        }
+        else {
+            throw "Invalid Zip Code";
+        }
+
+        if (regex_for_phoneNumber.test(params[6])) {
+            this.phoneNumber = params[6];            
+        }
+        else {
+            throw "Invalid Phone Number";
+        }
+
+        if (regex_for_email.test(params[7])) {
+            this.email = params[7];
+        }
+        else {
+            throw "Invalid Email";
+        }
     }
 
     toString() {
@@ -32,5 +67,10 @@ class AddressBook {
 
 }
 
-let addressbook = new AddressBook("Surendra", "Chouhan", "Wadala", "Mumbai", "Maharashtra", 400037, 9987451480, "chouhansurendra88@gmail.com");
-console.log(addressbook.toString());
+try {
+    let addressbook = new AddressBook("Surendra", "Chouhan", "Wadala", "Mumbai", "Maharashtra", 400037, 9987451480, "chouhansurendra88@gmail.com");
+    console.log(addressbook.toString());
+}
+catch (e) {
+    console.error(e);
+}
